8468661487aaf9d4f3e525168124e02e157b8380
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-06-12 17:27:06 +0530

    footer comlete

--- index/footer.php
+++ index/footer.php
@@ -1,35 +1,74 @@
 <footer>
-    <div class="footer-column footer-img">
-      <img src="./img/logo.svg" alt="Logo" width="302px" class="footer-logo" style="padding-bottom: 32px;">
-      <img src="./img/logo2.svg" class="footer-logo-parent " alt="Logo" ><br>
-      <a href="https://www.kirtii.com" class="footer-link">https://www.kirtii.com</a>
-    </div>
+  <div class="footer-column footer-img">
+    <img src="./img/logo.svg" alt="Logo" width="302px" class="footer-logo" style="padding-bottom: 32px;">
+    <img src="./img/logo2.svg" class="footer-logo-parent " alt="Logo"><br>
+    <a href="https://www.kirtii.com" class="footer-link">https://www.kirtii.com</a>
+  </div>
 
 
-    <div class="footer-column">
-        <div class="location">
-        <img src="./img/location-icon.png" alt=""> <h4>Contact Us</h4></div>
-      <p>VESU:</p>
-      <p>108-109-110 SNS Atria - A, Rundh, </p>
-      <p> Vesu,Surat- 394518, Gujarat, India</p>
-
-    </div>
-    <div class="footer-column">
-        <h4>Links</h4>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
+  <div class="footer-column">
+    <h4>Contact Us</h4>
+    <div class="contact-info">
+      <img src="./img/location-icon.png" alt="Location Icon">
+      <div class="text-info">
+        <p>VESU:</p>
+        <p>108-109-110 SNS Atria - A, Rundh,</p>
+        <p>Vesu, Surat-394518, Gujarat, India</p>
       </div>
-
-    <div class="footer-column">
-        <h4>Newsletter</h4>
-        <p>Subscribe to our newsletter to receive updates.</p>
-        <form action="#">
-          <input type="email" placeholder="Enter your email" required>
-          <button type="submit">Subscribe</button>
-        </form>
+    </div>
+    <div class="contact-info">
+      <img src="./img/location-icon.png" alt="Location Icon">
+      <div class="text-info">
+        <p>ADAJAN:</p>
+        <p>4, Premjinagar Society-1, Near,
+        <p>Sardar Bridge Circle,Opp.</p>
+        <p> Muktanand Complex, Adajan ,</p>
+        <p>Surat-395009, Gujarat, India</p>
       </div>
-  </footer>
+    </div>
+    <div class="contact-info">
+      <img src="./img/location-icon.png" alt="Location Icon">
+      <div class="text-info">
+        <p>CHAUTAPUL:</p>
+        <p>1st Floor, Sahebji </p>
+        <p>Shelter,Chautapul,Surat- 395003</p>
+        <p>Gujarat, India</p>
+      </div>
+    </div>
+  </div>
+  <div class="footer-column">
+    <h4>Pages</h4>
+    <ul>
+    <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact Us</a></li>
+      <li><a href="#">Team</a></li>
+      <li><a href="#">Testimonials</a></li>
+    </ul>
+  </div>
+
+  <div class="footer-column">
+    <h4>Subscribe</h4>
+    <form action="#">
+      <input type="email" placeholder="Enter your email" required>
+      <button type="submit">Subscribe</button>
+    </form>
+    <div class="follow-us">
+      <h4>Follow Us</h4>
+      <a href="#"><img src="./img/Instagram.png" alt=""></a>
+      <a href="#"><img src="./img/facebook.png" alt=""></a>
+      <a href="#"><img src="./img/gmail.png" alt=""></a>
+      <a href="#"><img src="./img/phone.png" alt=""></a>
+
+
+    </div>
+    <a href="mailto:customercare@beautyncurves.com" class="email-link">
+      <img src="./img/email.png" alt="">
+      <span>customercare@beautyncurves.com</span>
+    </a>
+
+    <a href="tel:1800-572-3579"> Tel: 1800-572-3579</a>
+  </div>
+
+
+</footer>
