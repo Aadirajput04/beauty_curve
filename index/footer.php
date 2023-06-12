094ccd7805aa5f7d8d117154f98b5dbab32d9a71
aaditya singh <aadityasingh396@gmail.com>
2023-06-12 15:59:19 +0530

    blueprint

---
+++ index/footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="footer-column footer-img">
+      <img src="./img/logo.svg" alt="Logo" width="302px" class="footer-logo" style="padding-bottom: 32px;">
+      <img src="./img/logo2.svg" class="footer-logo-parent " alt="Logo" ><br>
+      <a href="https://www.kirtii.com" class="footer-link">https://www.kirtii.com</a>
+    </div>
+
+
+    <div class="footer-column">
+        <div class="location">
+        <img src="./img/location-icon.png" alt=""> <h4>Contact Us</h4></div>
+      <p>VESU:</p>
+      <p>108-109-110 SNS Atria - A, Rundh, </p>
+      <p> Vesu,Surat- 394518, Gujarat, India</p>
+
+    </div>
+    <div class="footer-column">
+        <h4>Links</h4>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+
+    <div class="footer-column">
+        <h4>Newsletter</h4>
+        <p>Subscribe to our newsletter to receive updates.</p>
+        <form action="#">
+          <input type="email" placeholder="Enter your email" required>
+          <button type="submit">Subscribe</button>
+        </form>
+      </div>
+  </footer>
---
+++ index/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+
+    <?php
+    include "head_lable.php";
+
+
+     include "footer.php";
+
+
+    ?>
+
+    <script src="javascript/main.js"></script>
+</body>
+</html>
