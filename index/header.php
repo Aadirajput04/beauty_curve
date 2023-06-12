e44956b734928f265bfd42eb6e1f17eb684cad57
aaditya singh <aadityasingh396@gmail.com>
2023-06-12 16:32:15 +0530

    header created

--- index/header.php
+++ index/header.php
@@ -1,4 +1,24 @@
-<marquee direction="left"behavior="alternate">customercare@beautyncurves.com
+<marquee direction="left" bgcolor="#F5EFF3"  behavior="alternate">customercare@beautyncurves.com
     <span>TOLL FREE: 1800-572-3579</span>
-    <span></span>
+    <span><a href=""> <img src="./img/Instagram.svg" alt=""> </a><a href=""> <img src="./img/Facebook.svg" alt=""> </a><a href=""> <img src="./img/Gmail.svg" alt=""> </a><a href=""> <img src="./img/Phone.svg" alt=""> </a></span>
 </marquee>
+
+
+
+<header>
+    <div class="logo">
+      <img src="./img/logo.svg" alt="Logo">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#">home</a></li>
+        <li><a href="#">about</a></li>
+        <li><a href="#">services </a></li>
+        <li><a href="#">contact us</a></li>
+        <li><a class="button" href="#">Book apointment</a></li>
+        
+      </ul>
+    </nav>
+   
+  </header>
+  
