f76b77f3d4bee9373a61de8b9f7fc58ffaf2786a
aaditya singh <aadityasingh396@gmail.com>
2023-06-12 16:09:51 +0530

    created

---
+++ index/header.php
@@ -0,0 +1,4 @@
+<marquee direction="left"behavior="alternate">customercare@beautyncurves.com
+    <span>TOLL FREE: 1800-572-3579</span>
+    <span></span>
+</marquee>
--- index/index.php
+++ index/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
     <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
+
 <body>
 
 
@@ -15,14 +17,15 @@
 
 
     <?php
-    include "head_lable.php";
+    include "header.php";
 
 
-     include "footer.php";
+    include "footer.php";
 
 
     ?>
 
     <script src="javascript/main.js"></script>
 </body>
+
 </html>
