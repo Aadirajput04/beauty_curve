b0f9bef835a4e0dcd10dccc933382cbc0fa87046
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-06-14 17:35:18 +0530

    home page half completed

--- index/index.php
+++ index/index.php
@@ -17,9 +17,47 @@
 
 
     <?php
-    
-    include "header.php";
 
+
+
+
+    include "header.php"; ?>
+
+
+
+    <div class="section-main">
+        <div class="main-section-hero">
+            <div class="section-hero-left">
+                <h2>Embrace Your<br>
+                    Authentic <br>Beauty</h2>
+                <p>Journey to Unveiling Your Radiance</p>
+                <a class="button" href="#">Book Apointment</a>
+
+            </div>
+            <div class="section-hero-right">
+
+            </div>
+        </div>
+        <div class="section-hero2">
+            <div class="view">
+
+
+
+                <h1>We are the leading<br>
+                    skin centre in Gujarat</h1>
+                <div class="view-more">
+                    <h2>View More <img class="arrow-img" src="./img/ArrowDown.svg" alt=""> </h2>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    <?php
 
     include "footer.php";
 
